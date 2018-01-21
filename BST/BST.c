11b791a9f148004bd620c434a742535f9ba08f76
DaJuice <ojohnsto@tcd.ie>
2018-01-21 19:07:07 +0000

    Finished up the BST

--- BST/BST.c
+++ BST/BST.c
@@ -27,6 +27,16 @@ Node* find_min(Node* node){
   }
 }
 
+// Helper function to find the max value in a tree
+Node* find_max(Node* node) {
+  if(node->right == NULL){
+    return node;
+  }
+  else {
+    return find_max(node->right);
+  }
+}
+
 // Helper function to do an inorder traversal of the binary tree
 void inorder(struct node* root){
   if(root != NULL){
@@ -37,7 +47,7 @@ void inorder(struct node* root){
 }
 
 Node* insert_node(Node* node, int data) {
-  if(node == NULL) { create_node(data); }
+  if(node == NULL) { return create_node(data); }
   if(data < node->data){
     node->data = insert_node(node->left, data);
   }
@@ -50,16 +60,74 @@ Node* insert_node(Node* node, int data) {
 Node* delete_node(Node* node, int data) {
   if(node == NULL){
     fprintf(stderr, "ERR: Can't delete from an empty tree");
+    return root;
   }
-  if(data == node->data){
-    // Three cases
-    // #1 is if the tree has one child
-    if(node->left == NULL && node->right == NULL){
-      
+
+  // If key is less, go into the left subtree
+  if(data < node->data){
+    node->left = delete_node(node->left, data);
+  }
+  // If key is greater, go into the right subtree
+  else if(data > node->data){
+    node->right = delete_node(node->right, data);
+  }
+  // Found the node!
+  else {
+
+    if(node->left == NULL){
+      Node* temp = node->right;
+      free(node);
+      return temp;
     }
+    else if(node->right == NULL){
+      Node* temp = node->left;
+      free(node);
+      return temp;
+    }
+
+    // for a node with two children
+    Node* temp = find_min(node->right);
+    // copy in-order successor's content to this node
+    node->data = temp->data;
+    // delete the in-order successor
+    node->right = delete_node(node->right, temp->data);
   }
+  return node;
 }
 
 int main() {
+  /* Let us create following BST
+           50
+        /     \
+       30      70
+      /  \    /  \
+    20   40  60   80 */
+  struct node *root = NULL;
+  root = insert(root, 50);
+  root = insert(root, 30);
+  root = insert(root, 20);
+  root = insert(root, 40);
+  root = insert(root, 70);
+  root = insert(root, 60);
+  root = insert(root, 80);
+
+  printf("Inorder traversal of the given tree \n");
+  inorder(root);
+
+  printf("\nDelete 20\n");
+  root = delete_node(root, 20);
+  printf("Inorder traversal of the modified tree \n");
+  inorder(root);
+
+  printf("\nDelete 30\n");
+  root = delete_node(root, 30);
+  printf("Inorder traversal of the modified tree \n");
+  inorder(root);
+
+  printf("\nDelete 50\n");
+  root = delete_node(root, 50);
+  printf("Inorder traversal of the modified tree \n");
+  inorder(root);
+
   return 0;
 }
