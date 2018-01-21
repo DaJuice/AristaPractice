b1be88620f602e7965c984348205f7f919a01f7e
DaJuice <ojohnsto@tcd.ie>
2018-01-21 22:02:06 +0000

    changed boggle to c++ cause strings in C are hard

---
+++ Boggle/Boggle(Graph).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+// Implementation of Boggle using a "graph" based approach.
+// The solution uses a Depth First Traversal of the NxN grid.
+
+
+
+bool isWord(char word[]){
+
+}
+
+int main() {
+  FILE* dictionary = fopen("./words", "r");
+
+  return 0;
+}
